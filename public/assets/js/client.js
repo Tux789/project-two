4c536ff770b09fc87f6f49bf7993ceed3fb08a5d
laurawilsone <laura.wils204@gmail.com>
2018-06-14 21:22:50 -0500

    manager

---
+++ public/assets/js/client.js
@@ -0,0 +1,16 @@
+// get, put, and post for backend
+
+//for order, manager, and invoice
+
+// quantity and item are dynamic
+
+// quantity per pound
+
+
+
+// submit to a post to the backend 
+
+//routes 
+ //for invoices: "/api/invoice"
+ //for products: "/api/products"
+ //for charts: "/api/charts"
