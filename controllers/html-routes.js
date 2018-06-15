4689f88f507f76c6bd59de5aa40edddb26e77e4d
Jeff Swafford <jlswafford789@yahoo.com>
2018-06-14 20:38:16 -0500

    Updated Invoice, Added chart and Email on backend

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,5 +1,25 @@
 var path = require("path")
 
 module.exports= function(app){
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      });
 
+    app.get("/invoice", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/invoice.html"));
+    });
+
+    app.get("/manager", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/manager.html"));
+    });
+
+    app.get("/charts", function(req, res) {
+        //res.sendFile(path.join(__dirname, "../public/charts.html"));
+        res.sendFile(path.join(__dirname, "../public/chartTest.html"));
+    });
+
+
+    app.get("/order", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/order.html"));
+    });
 };
