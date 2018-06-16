153e116894ffa76af63d3b28138605727aad27a4
DESKTOP-DE5BIT9\hxx14 <xhrsteven@gmail.com>
2018-06-16 13:10:50 -0500

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,22 @@
-var authController = require('../controllers/authcontroller');
+var authController = require('../controllers/authcontroller.js');
 
 module.exports = function(app, passport){
     app.get('/signup', authController.signup);
 
     app.get('/signin', authController.signin);
 
-    app.post('/signup', passport.authController('local-signup',
-    { successRedirect: '/signin',
-    failureRedirect: '/signup'}
+    app.post('/signup', passport.authenticate('local-signup',
+        { successRedirect: '/signin',
+        failureRedirect: '/signup'}
     ));
 
-    app.get('/',isLoggedIn, authController.dashboard);
+    app.get('/dashboard',isLoggedIn, authController.dashboard);
 
     app.get('/logout',authController.logout);
 
     app.post('/signin', passport.authenticate('local-signin',  
-    { successRedirect: '/dashboard',                                              
-        failureRedirect: '/signin'} ));
+        { successRedirect: '/index',                                              
+          failureRedirect: '/signin'} ));
 
 
     function isLoggedIn(req, res, next) {
